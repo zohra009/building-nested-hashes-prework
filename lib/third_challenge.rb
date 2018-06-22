0a50c170dcc5294925a7b3d4cd12d336d56fe8bc
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:42:09 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name "Lord Montague", age: 
+        name: "Lord Montague", age: "35"
         },
       :matriarch => {
         #your key/value pairs here
