2fa117c210980cfd37928054ff8b70772f344237
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:45:28 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
         name: "Lady Montague", age: "54"
         },
       :hero => {
-        name: "Romeo", age: "15"
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
