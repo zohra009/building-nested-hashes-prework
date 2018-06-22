28a991e1c8ca7ebcb81647d4991607a9cfa32567
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:38:38 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
