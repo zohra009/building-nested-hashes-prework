d34207d9bb91a84ff46a0dede9859e74fb6b95b8
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:39:41 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
