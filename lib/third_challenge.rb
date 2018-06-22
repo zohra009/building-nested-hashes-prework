4ed522909a759e9409aef40d459d4002cadd8546
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:46:51 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,7 @@ def third_challenge
         name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliette", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
