ddf94017a747f12431faa0889b56136502043525
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:46:20 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
         name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        name: "Lord Capulet", age: "50"
+        #your key/value pairs here
         },
       :heroine => {
         #your key/value pairs here
